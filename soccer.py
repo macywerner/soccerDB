5bd025d5b14a922199cb9fc478956a2a53cad2e8
Macy Werner <mwerner@chapman.edu>
2021-12-14 13:03:04 -0800

    Update soccer.py

--- soccer.py
+++ soccer.py
@@ -5,12 +5,14 @@ from ops import db_operations
 # Uses st.cache to only run once.
 #@st.cache(allow_output_mutation=True, hash_funcs={"_thread.RLock": lambda _: None})
 
+db_ops = db_operations()
+
 
 # Perform query.
 # Uses st.cache to only rerun when the query changes or after 10 min.
 @st.cache(ttl=600)
 
-rows = run_query("SELECT * from country;")
+rows = db_ops.run_query("SELECT * from country;")
 
 # Print results.
 st.write(f"{row[0]}")
