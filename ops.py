0157a3edf356eca9bb0b1b00349ee832d1836210
Macy Werner <mwerner@chapman.edu>
2021-12-14 13:01:53 -0800

    added ops

---
+++ ops.py
@@ -0,0 +1,19 @@
+import mysql.connector
+
+
+class db_operations():
+    def __init__(self): # constructor with connection path to db
+        self.connection = mysql.connector.connect(
+        **st.secrets["mysql"]
+        )
+        self.cursor = self.connection.cursor()
+        print("connection made..")
+
+    def bulk_insert(self,query,records):
+        self.cursor.executemany(query,records)
+        self.connection.commit()
+        print("query executed..")
+
+    def run_query(query):
+        self.cursor.execute(query)
+        return self.cursor.fetchall()
--- soccer.py
+++ soccer.py
@@ -1,22 +1,14 @@
 import streamlit as st
-import mysql.connector
+from ops import db_operations
 
 # Initialize connection.
 # Uses st.cache to only run once.
 #@st.cache(allow_output_mutation=True, hash_funcs={"_thread.RLock": lambda _: None})
-def init_connection(self):
-    self.connection = mysql.connector.connect(**st.secrets["mysql"])
-    self.cursor = self.connection.cursor()
 
-conn = init_connection()
 
 # Perform query.
 # Uses st.cache to only rerun when the query changes or after 10 min.
 @st.cache(ttl=600)
-def run_query(query):
-    with conn.cursor() as cur:
-        cur.execute(query)
-        return cur.fetchall()
 
 rows = run_query("SELECT * from country;")
 
